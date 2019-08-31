94c63baf205904fe77a5be00df8618e8f6f7f02e
JustusFowl <justusfowl@gmail.com>
2019-08-31 13:57:35 -0500

    Updated UI

--- server.R
+++ server.R
@@ -4,6 +4,9 @@ library(dplyr)
 
 source("helper.R")
 
+
+load("data/APPL.Rda")
+
 # read input data from model evaluation
 
 df_model_results <- read.csv("models.csv")
@@ -28,6 +31,16 @@ get_data <- function(ticker=""){
   
 }
 
+get_data_foundation <- function(ticket=""){
+  
+  actuals <- rnorm(100, mean = 5)
+  x <- c(1:100)
+  date <- seq(ISOdate(2018,1,1), by = "days", length.out = 100)
+  
+  data <- data.frame(x, date, actuals)
+  data
+}
+
 get_stored_model_results <- function(ticker=""){
   if (ticker == ""){
     return
@@ -49,6 +62,8 @@ server <- function(input, output) {
   output$predictChart <- renderPlotly({
     
     ticker <- input$predictTicker
+    modelhighPerforming <- input$predSelectRecommendedModel
+    modelVar <- input$modelTypeSelect
     
     # get data by ticker and load models --> execute predictions
     
@@ -77,8 +92,19 @@ server <- function(input, output) {
   
   output$predictTickerModelSelected <- renderValueBox({
     sel_model_results <- get_stored_model_results(input$predictTicker)
+    
+    if(input$predSelectRecommendedModel){
+      # get the one which is stored as best performing model for ticker
+      sel_model_results <- get_stored_model_results(input$predictTicker)
+      display <- sel_model_results$ModelType
+      
+    }else{
+      sel_model_results <- input$modelTypeSelect
+      display <- sel_model_results
+    }
+    
     valueBox(
-      paste0(sel_model_results$ModelType), "MODEL", icon = icon("microchip"),
+      paste0(display), "MODEL", icon = icon("microchip"),
       color = "blue"
     )
   })
@@ -92,6 +118,36 @@ server <- function(input, output) {
   })
   
   output$outPredTable <- renderDataTable(df_pred_data)
-
   
+  # model evaluation
+  
+  output$txt_gen_approach <- renderText({"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vitae leo in tellus imperdiet posuere. In fringilla neque faucibus velit vulputate, venenatis congue dolor gravida. Quisque posuere viverra cursus. Duis sapien metus, dapibus et tristique non, egestas eget dui. Ut et ante tortor. Aliquam erat volutpat. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam felis quam, ullamcorper a rutrum id, tristique a tortor. Duis sem turpis, interdum in euismod at, ornare vel massa. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec viverra rhoncus aliquet. Praesent arcu turpis, volutpat nec risus in, cursus vehicula urna. Proin in tristique libero. Nam eleifend metus a fermentum ornare. Donec mauris mauris, sagittis nec leo tincidunt, pretium venenatis turpis. Morbi mattis ultricies purus, vitae scelerisque justo vulputate eu. Aliquam bibendum tellus sed lacinia dictum. Aliquam erat volutpat. Phasellus faucibus pretium nunc dictum condimentum. Duis congue mattis nisi volutpat tincidunt. Mauris tincidunt purus non lacus fermentum tempor. Sed bibendum vitae urna vitae porttitor. Suspendisse erat ipsum, viverra a tincidunt ut, venenatis eget orci. Mauris id ante eget massa iaculis varius a euismod velit. Pellentesque in felis quis odio rhoncus fermentum sed vitae turpis. Etiam et suscipit lorem, non posuere purus. Nullam semper eleifend metus ut consequat. Cras auctor mi sapien, at consequat lacus semper ut. Curabitur ornare convallis dui vitae vehicula. Sed congue quam eu consectetur accumsan. Curabitur non auctor magna."})
+
+  output$modEvalChart <- renderPlotly({
+    
+    ticker <- input$modEvalTicker
+    
+    df_pred_data <- get_data_foundation(ticker)
+    
+    p <- plot_ly(df_pred_data, x = ~date, y = ~actuals, name = 'Actuals', type = 'scatter', mode = 'lines', source = "subset")
+    p
+    
+  })
+  
+  output$txt_mod_data_summary <- renderText({
+    ticker <- input$modEvalTicker
+    summary(df_pred_data)
+  })
+  
+  output$txt_mod_details <- renderText({
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vitae leo in tellus imperdiet posuere. In fringilla neque faucibus velit vulputate, venenatis congue dolor gravida. Quisque posuere viverra cursus. Duis sapien metus, dapibus et tristique non, egestas eget dui. Ut et ante tortor. Aliquam erat volutpat. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam felis quam, ullamcorper a rutrum id, tristique a tortor. Duis sem turpis, interdum in euismod at, ornare vel massa. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec viverra rhoncus aliquet. Praesent arcu turpis, volutpat nec risus in, cursus vehicula urna. Proin in tristique libero. Nam eleifend metus a fermentum ornare. Donec mauris mauris, sagittis nec leo tincidunt, pretium venenatis turpis. Morbi mattis ultricies purus, vitae scelerisque justo vulputate eu. Aliquam bibendum tellus sed lacinia dictum. Aliquam erat volutpat. Phasellus faucibus pretium nunc dictum condimentum. Duis congue mattis nisi volutpat tincidunt. Mauris tincidunt purus non lacus fermentum tempor. Sed bibendum vitae urna vitae porttitor. Suspendisse erat ipsum, viverra a tincidunt ut, venenatis eget orci. Mauris id ante eget massa iaculis varius a euismod velit. Pellentesque in felis quis odio rhoncus fermentum sed vitae turpis. Etiam et suscipit lorem, non posuere purus. Nullam semper eleifend metus ut consequat. Cras auctor mi sapien, at consequat lacus semper ut. Curabitur ornare convallis dui vitae vehicula. Sed congue quam eu consectetur accumsan. Curabitur non auctor magna."
+  })
+  
+  output$txt_googl_mod_eval <- renderText({
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vitae leo in tellus imperdiet posuere. In fringilla neque faucibus velit vulputate, venenatis congue dolor gravida. Quisque posuere viverra cursus. Duis sapien metus, dapibus et tristique non, egestas eget dui. Ut et ante tortor. Aliquam erat volutpat. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam felis quam, ullamcorper a rutrum id, tristique a tortor. Duis sem turpis, interdum in euismod at, ornare vel massa. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec viverra rhoncus aliquet. Praesent arcu turpis, volutpat nec risus in, cursus vehicula urna. Proin in tristique libero. Nam eleifend metus a fermentum ornare. Donec mauris mauris, sagittis nec leo tincidunt, pretium venenatis turpis. Morbi mattis ultricies purus, vitae scelerisque justo vulputate eu. Aliquam bibendum tellus sed lacinia dictum. Aliquam erat volutpat. Phasellus faucibus pretium nunc dictum condimentum. Duis congue mattis nisi volutpat tincidunt. Mauris tincidunt purus non lacus fermentum tempor. Sed bibendum vitae urna vitae porttitor. Suspendisse erat ipsum, viverra a tincidunt ut, venenatis eget orci. Mauris id ante eget massa iaculis varius a euismod velit. Pellentesque in felis quis odio rhoncus fermentum sed vitae turpis. Etiam et suscipit lorem, non posuere purus. Nullam semper eleifend metus ut consequat. Cras auctor mi sapien, at consequat lacus semper ut. Curabitur ornare convallis dui vitae vehicula. Sed congue quam eu consectetur accumsan. Curabitur non auctor magna."
+  })
+  
+  output$txt_appl_mod_eval <- renderText({
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vitae leo in tellus imperdiet posuere. In fringilla neque faucibus velit vulputate, venenatis congue dolor gravida. Quisque posuere viverra cursus. Duis sapien metus, dapibus et tristique non, egestas eget dui. Ut et ante tortor. Aliquam erat volutpat. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam felis quam, ullamcorper a rutrum id, tristique a tortor. Duis sem turpis, interdum in euismod at, ornare vel massa. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec viverra rhoncus aliquet. Praesent arcu turpis, volutpat nec risus in, cursus vehicula urna. Proin in tristique libero. Nam eleifend metus a fermentum ornare. Donec mauris mauris, sagittis nec leo tincidunt, pretium venenatis turpis. Morbi mattis ultricies purus, vitae scelerisque justo vulputate eu. Aliquam bibendum tellus sed lacinia dictum. Aliquam erat volutpat. Phasellus faucibus pretium nunc dictum condimentum. Duis congue mattis nisi volutpat tincidunt. Mauris tincidunt purus non lacus fermentum tempor. Sed bibendum vitae urna vitae porttitor. Suspendisse erat ipsum, viverra a tincidunt ut, venenatis eget orci. Mauris id ante eget massa iaculis varius a euismod velit. Pellentesque in felis quis odio rhoncus fermentum sed vitae turpis. Etiam et suscipit lorem, non posuere purus. Nullam semper eleifend metus ut consequat. Cras auctor mi sapien, at consequat lacus semper ut. Curabitur ornare convallis dui vitae vehicula. Sed congue quam eu consectetur accumsan. Curabitur non auctor magna."
+  })
 }
--- ui.R
+++ ui.R
@@ -5,13 +5,10 @@ dashboardPage(
   dashboardHeader(title = "Stock Prediction"),
   dashboardSidebar(
     sidebarMenu(
-      selectInput("predictTicker", "TICKER:",
-                  c("APPLE" = "APPL",
-                    "GOOGLE" = "GOOGL",
-                    "SIEMENS" = "SIEGY")),
+
       menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
       menuItem("Settings", tabName = "settings", icon = icon("gear")), 
-      menuItem("Settings", tabName = "settings", icon = icon("gear"))
+      menuItem("Evaluation", tabName = "eval", icon = icon("search"))
       
     )
   ),
@@ -37,28 +34,91 @@ dashboardPage(
       ),
       
       # Second tab content
-      tabItem(tabName = "SETTINGS",
-              h2("SELECT TICKER"), 
-              fluidRow(
-                box(
-                  selectInput("variable", "Variable:",
-                              c("Cylinders" = "cyl",
-                                "Transmission" = "am",
-                                "Gears" = "gear"))
-                )
-                
-              )
-      ),
       tabItem(tabName = "settings",
-              h2("SELECT TICKER"), 
               fluidRow(
-                box(
-                  selectInput("variable", "Variable:",
-                              c("Cylinders" = "cyl",
-                                "Transmission" = "am",
-                                "Gears" = "gear"))
+                box(width=12,
+                  selectInput("predictTicker", h3("TICKER:"),
+                              c("APPLE" = "APPL",
+                                "GOOGLE" = "GOOGL",
+                                "SIEMENS" = "SIEGY"))
                 )
-                
+              ),
+              fluidRow(
+                box(width=12,
+                    dateRangeInput("dates", h3("FORECASTING PERIOD:"))
+                )
+              ),
+              fluidRow(
+                box(width=12,
+                    h3("FORECASTING PERIOD:"),
+                    checkboxInput("predSelectRecommendedModel","Use best performing model (recommended)")
+                )
+              ),
+              fluidRow(
+                box(width=12,
+                    radioButtons("modelTypeSelect", "manual selection",
+                                c("Random Forest" = "RF",
+                                  "SVM" = "SVM",
+                                  "NB" = "NB",
+                                  "Moving averages" = "MA"))
+                )
+              ), 
+              menuItem("Predict", tabName = "dashboard", icon = icon("dashboard"))
+      ),
+      tabItem(tabName = "eval",
+              h2("GENERAL APPROACH"), 
+              fluidRow(
+                box( width=12,
+                  textOutput("txt_gen_approach")
+                )
+              ),
+              h2("DATA FOUNDATION"), 
+              fluidRow(
+                box(width=3, selectInput("modEvalTicker", "TICKER:",
+                                         c("APPLE" = "APPL",
+                                           "GOOGLE" = "GOOGL",
+                                           "SIEMENS" = "SIEGY")))
+              ),
+              fluidRow(
+                box( width=8,
+                     plotlyOutput("modEvalChart")
+                ), 
+                box( width=4, 
+                     textOutput("txt_mod_data_summary")
+                     )
+              ), 
+              h1("MODELLING"), 
+              h3("TRAINING"),
+              fluidRow(
+                box( width=12,
+                     h4("Feature engineering"),
+                     textOutput("txt_mod_details"), 
+                     code("Code for features created")
+                )
+              ),
+              
+              h3("EVALUATION & SELECTION"),
+
+              ## THIS SECTION NEEDS TO BE CONDITIONAL UPON THE SELECTED TICKER ## 
+              
+              # GOOGL
+              
+              fluidRow(
+                box( width=12,
+                     h4("Model training & evaluation | GOOGL"),
+                     textOutput("txt_googl_mod_eval"), 
+                     code("Code for features created")
+                )
+              ),
+              
+              # APPL
+              
+              fluidRow(
+                box( width=12,
+                     h4("Model training & evaluation | GOOGL"),
+                     textOutput("txt_appl_mod_eval"), 
+                     code("Code for features created")
+                )
               )
       )
     )
